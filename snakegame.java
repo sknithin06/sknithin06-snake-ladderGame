61574827ef625f0cd042c35799525d9d6bde8c70
sknithin06 <nithinsk939@gmail.com>
2021-07-03 13:44:24 +0530

    UC2 is added

--- snakegame.java
+++ snakegame.java
@@ -1,6 +1,12 @@
 package Java;
+import java.util.Scanner;
 public class snakegame {
 	public static void main(String[] args) { 
 		System.out.println("Welcome to Snake & Ladder Game");
+		
+		Scanner num = new Scanner(System.in);
+		System.out.print("Enter Starting position of the game: ");
+		int  playerStartPosition = num.nextInt();
+		System.out.println("Player Starting Position is:"+playerStartPosition);
 	}
 }
